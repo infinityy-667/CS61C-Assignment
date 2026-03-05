ad281920e9c2fff003a81acd07ee48bfb40c409a
infinityy-667 <3485342217@qq.com>
2026-03-05 11:42:15 +0800

    Update bit_ops.c

--- Lab/lab02/bit_ops.c
+++ Lab/lab02/bit_ops.c
@@ -5,23 +5,35 @@
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
                  unsigned n) {
+    unsigned mask = 1;
+    mask << n;
+    unsigned ans =  mask & x;
     // YOUR CODE HERE
+    
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    return -1;            
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
+    unsigned mask = ~(1 << n);
+    *x = *x & mask;
+    v = v << n;
+    *x = *x & v;
     // YOUR CODE HERE
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
+    unsigned mask = 1;
+    mask = mask << n;
+    *x = *x ^ mask;
+    
     // YOUR CODE HERE
 }
 
