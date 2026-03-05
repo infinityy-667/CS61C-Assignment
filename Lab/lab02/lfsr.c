72402bf4c9d9c79ff3b543d5a7a25f6c0f9fd5f8
infinityy-667 <3485342217@qq.com>
2026-03-05 13:57:37 +0800

    Update lfsr.c

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -6,6 +6,13 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
-    unsigned 
+    uint16_t origin = *reg; 
+    uint16_t bit0 = origin << 0;
+    uint16_t bit2 = origin << 2;
+    uint16_t bit3 = origin << 3;
+    uint16_t bit5 = origin << 5;
+    uint16_t newbit = bit0 ^ bit2 ^ bit3 ^ bit5;
+    *reg = *reg >> 1;
+    *reg |= newbit;
 }
 
