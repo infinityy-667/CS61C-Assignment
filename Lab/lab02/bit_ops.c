0a420119ddeed7607ff85d2e19e77853b1c1ef45
infinityy-667 <3485342217@qq.com>
2026-03-08 15:10:05 +0800

    第二次修改

--- Lab/lab02/bit_ops.c
+++ Lab/lab02/bit_ops.c
@@ -5,25 +5,24 @@
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
                  unsigned n) {
-    unsigned mask = 1;
-    mask << n;
-    unsigned ans =  mask & x;
+    return (x >> n) & 1;
     // YOUR CODE HERE
     
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;            
+    return ans ? 1 : 0;            
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    unsigned mask = ~(1 << n);
-    *x = *x & mask;
-    v = v << n;
-    *x = *x & v;
+    unsigned mask = 1 << n;
+
+    *x = *x & ~mask;//清0这一位
+    unsigned set = mask * v;//把v提高到第n位的位置
+    *x = set | *x;
     // YOUR CODE HERE
 }
 // Flip the nth bit of the value of x.
