9543c55ea295db480ba4af5cac3762722ce398d0
infinityy-667 <3485342217@qq.com>
2026-03-12 20:25:04 +0800

    risc v

--- Lab/lab03/factorial.s
+++ Lab/lab03/factorial.s
@@ -6,10 +6,10 @@ n: .word 8
 .text
 main:
     la t0, n
-    lw a0, 0(t0)
+    lw a0, 0(t0) #a0 == 8
     jal ra, factorial
 
-    addi a1, a0, 0
+    mv a1, a0
     addi a0, x0, 1
     ecall # Print Result
 
@@ -22,3 +22,20 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    li t1, 1 #初始化为1
+    li t2, 1 #来记录大小
+    addi t3, a0, 1 #9
+    
+loop:
+    bge t1, t3, Done # 到了9就退出
+    mul t2, t2, t1 #乘
+    addi t1, t1, 1 #自增
+    j loop 
+    
+    
+Done:
+    mv a0, t2 #赋值给a0
+    jalr ra #返回main
+    
+    
+    
